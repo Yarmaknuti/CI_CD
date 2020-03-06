926c41c2dfed112b658b9b55a834b348a69da009
Illia Dzmitryienka <i.dzmitryienka@godeltech.com>
2020-03-06 23:15:08 +0300

    begin

---
+++ src/main/java/godeltask/App.java
@@ -0,0 +1,13 @@
+package godeltask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
